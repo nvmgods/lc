6a1bc6f180fc47b6444681a143cb80bd9b42cfcb
nvmgods <kagami.nevermore@gmail.com>
2024-08-18 22:21:50 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows.Forms;
+using RavenDevLauncher.Interface;
+using System.Net;
+using System.IO;
+
+
+namespace RavenDevLauncher
+{
+    static class Program
+    {
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+            
+        {
+            
+                    Application.EnableVisualStyles();
+                    Application.SetCompatibleTextRenderingDefault(false);
+                    Application.Run(new Interface.FrmMain());
+                
+        }
+            
+    }
+}
+
+           
+            
+            
+            
+            
+
+
+
---
+++ Updater/Folders.cs
@@ -0,0 +1,14 @@
+﻿namespace RavenDevLauncher.Updater
+{
+    public class Folders
+    {
+        public string Source { get; private set; }
+        public string Target { get; private set; }
+
+        public Folders(string source, string target)
+        {
+            Source = source;
+            Target = target;
+        }
+    }
+}
